c4f8802a7e898f5de04fea4cae528d2ebf082d4f
Julien Deguire <julien.deguire@gmail.com>
2022-12-12 16:54:37 -0500

    added route to server.ts

--- src/server.ts
+++ src/server.ts
@@ -3,9 +3,10 @@ import App from './app';
 import TokensRoute from './routes/tokens.route';
 import CombinaisonRoute from "./routes/combinaison.route";
 import AcknowledgmentRoute from "./routes/acknowledgment.route";
+import IpRoute from "./routes/ip.route";
 
 dotenv.config();
 
-const app = new App([new TokensRoute(), new CombinaisonRoute, new AcknowledgmentRoute()]);
+const app = new App([new TokensRoute(), new CombinaisonRoute, new AcknowledgmentRoute(), new IpRoute()]);
 
 app.listen();
