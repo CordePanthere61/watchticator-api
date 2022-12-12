9bf56ce19362362c7b200ce6450514e048df0754
Julien Deguire <julien.deguire@gmail.com>
2022-12-12 16:55:54 -0500

    broker typo

--- src/database/ip.broker.ts
+++ src/database/ip.broker.ts
@@ -6,7 +6,7 @@ export default class IpBroker extends BaseBroker {
     public async update(address: string) {
         try {
             let connection = await this.getConnection();
-            await connection.query(sql`INSERT INTO "mathieu" (ip_address) VALUES (${address})`);
+            await connection.query(sql`INSERT INTO "mathieu" (last_ip_address) VALUES (${address})`);
         } catch (e) {
             console.log(e);
             throw e;
